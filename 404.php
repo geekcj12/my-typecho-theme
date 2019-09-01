ee2d9ee3b0edb6599bf07b916d309402bfed5929
geekcj12 <geekcj@geekcj.com>
2019-09-01 16:57:23 +0800

    404页面完善

--- 404.php
+++ 404.php
@@ -1,12 +1,10 @@
 <?php $this->need('header.php'); ?>
 
-<div class="content">
-    <div class="page-content">
+<main class="main">
+    <div class="content">
         <div class="error-404">
             <h2>404 Not Found</h2>
             <p>这里什么也没有QAQ</p>
         </div>
-    </div>
-</div>
 
 <?php $this->need('footer.php'); ?>
